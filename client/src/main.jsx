2b2f66652d6a9f33855cb38de5d041a355f43bd5
ahmadjon09 <youtobe83a61a@gmail.com>
2025-10-05 21:23:41 +0500

    V3

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './global.css'
 import './assets/css/nav.css'
-import { LanguageProvider } from './context/LanguageContext.jsx'
-import ContextProvider from './Context/Context.jsx'
+import ContextProvider from './Сontext/Context.jsx'
+import { LanguageProvider } from './Сontext/LanguageContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ContextProvider>
-    <LanguageProvider>
+  <LanguageProvider>
+    <ContextProvider>
       <App />
-    </LanguageProvider>
-  </ContextProvider>
+    </ContextProvider>
+  </LanguageProvider>
 )
