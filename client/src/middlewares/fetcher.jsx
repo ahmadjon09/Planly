5dfd48f5ac2cbf39eaa1b19fcc01d8bc3a28c652
ahmadjon09 <youtobe83a61a@gmail.com>
2025-10-05 21:19:47 +0500

    V2

--- client/src/middlewares/fetcher.jsx
+++ client/src/middlewares/fetcher.jsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 // const BASE_URL = `http://coludxuz.duckdns.org:7787/api`
-const BASE_URL = `http://localhost:7777/api`
-// const BASE_URL = `https://planly.onrender.com/api`
+// const BASE_URL = `http://localhost:7777/api`
+const BASE_URL = `https://planly.onrender.com/api`
 
 const token = Cookies.get('user_token')
 const instance = axios.create({
