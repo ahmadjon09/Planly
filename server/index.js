b29c247ac876ea16891a81002b26fe791b860c50
ahmadjon09 <youtobe83a61a@gmail.com>
2025-10-05 21:44:57 +0500

    V9

--- server/index.js
+++ server/index.js
@@ -4,15 +4,21 @@ import mongoose from 'mongoose'
 import axios from 'axios'
 import ProductRoutes from './routes/product.js'
 import OrderRoutes from './routes/order.js'
+import path from 'path'
 import UserRoutes from './routes/user.js'
 import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 dotenv.config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
 app.get('/api/status', (req, res) => {
   setImmediate(() => {
     res.json({
@@ -25,6 +31,13 @@ app.get('/api/', (_, res) => res.send('Server is running!'))
 app.use('/api/users', UserRoutes)
 app.use('/api/products', ProductRoutes)
 app.use('/api/orders', OrderRoutes)
+
+app.use((req, res) => {
+  if (!req.path.startsWith('/api')) {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
+  }
+})
+
 const keepServerAlive = () => {
   setInterval(() => {
     axios
